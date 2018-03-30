99fee7db19ab15437b583b4e2d621c3045725b70
Geir Gulbrandsen <dobbelg@gmail.com>
2018-03-30 21:31:15 +0100

    Inline temporary variable for sum

--- src/test/java/com/plusonetesting/rtdd/test/AddFractionTest.java
+++ src/test/java/com/plusonetesting/rtdd/test/AddFractionTest.java
@@ -10,31 +10,26 @@ public class AddFractionTest {
 
     @Test
     public void zeroPlusZero() {
-        Fraction sum = new Fraction(0).plus(new Fraction(0));
-        assertThat(sum.intValue() ,is(0));
+        assertThat(new Fraction(0).plus(new Fraction(0)).intValue() ,is(0));
     }
 
     @Test
     public void nonZeroPlusZero() {
-        Fraction sum = new Fraction(3).plus(new Fraction(0));
-        assertThat(sum.intValue(), is(3));
+        assertThat(new Fraction(3).plus(new Fraction(0)).intValue(), is(3));
     }
 
     @Test
     public void zeroPlusNonZero() {
-        Fraction sum = new Fraction(0).plus(new Fraction(5));
-        assertThat(sum.intValue(), is(5));
+        assertThat(new Fraction(0).plus(new Fraction(5)).intValue(), is(5));
     }
 
     @Test
     public void nonNegativeNonZeroOperands() {
-        Fraction sum = new Fraction(3).plus(new Fraction(4));
-        assertThat(sum.intValue(), is(7));
+        assertThat(new Fraction(3).plus(new Fraction(4)).intValue(), is(7));
     }
 
     @Test
     public void negativePlusPositiveWithNegativeResult() {
-        Fraction sum = new Fraction(-3).plus(new Fraction(1));
-        assertThat(sum.intValue(), is(-2));
+        assertThat(new Fraction(-3).plus(new Fraction(1)).intValue(), is(-2));
     }
 }
