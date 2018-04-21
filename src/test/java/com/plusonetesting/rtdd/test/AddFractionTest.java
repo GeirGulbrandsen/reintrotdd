cc88674104904752700c7aefecbbbccde3d387db
Geir Gulbrandsen <dobbelg@gmail.com>
2018-04-21 09:57:25 +0100

    Removed scaffolding in tests by using assertEquals

--- src/test/java/com/plusonetesting/rtdd/test/AddFractionTest.java
+++ src/test/java/com/plusonetesting/rtdd/test/AddFractionTest.java
@@ -2,44 +2,40 @@ package com.plusonetesting.rtdd.test;
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 
 public class AddFractionTest {
 
     @Test
     public void zeroPlusZero() {
-        assertThat(new Fraction(0).plus(new Fraction(0)).intValue() ,is(0));
+        assertEquals(new Fraction(0), new Fraction(0).plus(new Fraction(0)));
     }
 
     @Test
     public void nonZeroPlusZero() {
-        assertThat(new Fraction(3).plus(new Fraction(0)).intValue(), is(3));
+        assertEquals(new Fraction(3), new Fraction(3).plus(new Fraction(0)));
     }
 
     @Test
     public void zeroPlusNonZero() {
-        assertThat(new Fraction(0).plus(new Fraction(5)).intValue(), is(5));
+        assertEquals(new Fraction(5), new Fraction(0).plus(new Fraction(5)));
     }
 
     @Test
     public void nonNegativeNonZeroOperands() {
-        assertThat(new Fraction(3).plus(new Fraction(4)).intValue(), is(7));
+        assertEquals(new Fraction(7), new Fraction(3).plus(new Fraction(4)));
     }
 
     @Test
     public void negativePlusPositiveWithNegativeResult() {
-        assertThat(new Fraction(-3).plus(new Fraction(1)).intValue(), is(-2));
+        assertEquals(new Fraction(-2), new Fraction(-3).plus(new Fraction(1)));
     }
 
     @Test
-    public void nonTrivialButCommonDenominatior() {
-
-        Fraction sum = new Fraction(1, 5).plus(new Fraction(2, 5));
-
-        assertThat(sum.getNumerator(), is(3));
-        assertThat(sum.getDenominator(), is(5));
+    public void nonTrivialButCommonDenominator() {
+        assertEquals(new Fraction(3, 5),
+                new Fraction(1, 5).plus(new Fraction(2, 5)));
     }
 
 
