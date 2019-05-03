eaecfb98145803ec24a024c530e116884eed0090
Geir Gulbrandsen <dobbelg@gmail.com>
2019-05-03 17:28:46 +0100

    support fractions with different denominators as long as we don't need to reduce

--- src/main/java/com/plusonetesting/rtdd/test/Fraction.java
+++ src/main/java/com/plusonetesting/rtdd/test/Fraction.java
@@ -15,12 +15,18 @@ public class Fraction {
     }
 
     public Fraction plus(Fraction that) {
-        return (new Fraction(this.numerator + that.numerator, denominator));
+        if (this.denominator != that.denominator) {
+            return new Fraction(this.numerator * that.denominator + that.numerator * this.denominator,
+                    this.denominator * that.denominator);
+        } else {
+            return (new Fraction(this.numerator + that.numerator, denominator));
+        }
+
     }
 
     @Override
     public String toString() {
-        return String.format("%d/%d", numerator, denominator );
+        return String.format("%d/%d", numerator, denominator);
     }
 
     @Override
