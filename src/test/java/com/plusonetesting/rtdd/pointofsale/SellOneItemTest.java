ca615005180a98891efa5e85778cac62981d69c7
Geir Gulbrandsen <dobbelg@gmail.com>
2019-06-04 16:41:10 +0100

    We cn handle not finding a product

--- src/test/java/com/plusonetesting/rtdd/pointofsale/SellOneItemTest.java
+++ src/test/java/com/plusonetesting/rtdd/pointofsale/SellOneItemTest.java
@@ -25,6 +25,15 @@ public class SellOneItemTest {
         assertEquals("$12.50", display.getText());
     }
 
+    @Test
+    public void productNotFound() {
+        final Display display = new Display();
+        final Sale sale = new Sale(display);
+
+        sale.onBarcode("99999");
+        assertEquals("Product not found for 99999", display.getText()  );
+    }
+
     public static class Display {
         private String text;
 
@@ -46,8 +55,10 @@ public class SellOneItemTest {
         public void onBarcode(String barcode) {
             if ("12345".equals(barcode))
                 display.setText("$7.95");
-            else
+            else if ("23456".equals(barcode))
                 display.setText("$12.50");
+            else
+                display.setText("Product not found for 99999");
         }
     }
 }
