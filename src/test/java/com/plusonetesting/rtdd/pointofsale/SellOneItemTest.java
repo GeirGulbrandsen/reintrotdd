c1251a7eeac564911edec5eea12179df076c365a
Geir Gulbrandsen <dobbelg@gmail.com>
2019-06-04 16:28:54 +0100

    Sale now decides price

--- src/test/java/com/plusonetesting/rtdd/pointofsale/SellOneItemTest.java
+++ src/test/java/com/plusonetesting/rtdd/pointofsale/SellOneItemTest.java
@@ -1,27 +1,51 @@
 package com.plusonetesting.rtdd.pointofsale;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class SellOneItemTest {
     @Test
     public void productFound() {
         final Display display = new Display();
-        final Sale sale = new Sale();
+        final Sale sale = new Sale(display);
 
         sale.onBarcode("12345");
         assertEquals("$7.95", display.getText());
     }
 
+    @Test
+    @Ignore
+    public void anotherProductFound() {
+        final Display display = new Display();
+        final Sale sale = new Sale(display);
+
+        sale.onBarcode("23456");
+        assertEquals("12.50", display.getText());
+    }
+
     public static class Display {
+        private String text;
+
         public String getText() {
-            return "$7.95";
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
         }
     }
     public static class Sale {
-        public void onBarcode(String s) {
+        private Display display;
 
+        public Sale(Display display) {
+            this.display = display;
+        }
+
+        public void onBarcode(String s) {
+            display.setText("$7.95");
         }
     }
 }
