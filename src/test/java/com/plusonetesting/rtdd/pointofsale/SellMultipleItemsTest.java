577942efceeb6b675cb58a15296951d76011a592
Geir Gulbrandsen <dobbelg@gmail.com>
2019-06-18 17:01:56 +0100

    Apparently, we could already "sell" one item if we had no price for it.

--- src/test/java/com/plusonetesting/rtdd/pointofsale/SellMultipleItemsTest.java
+++ src/test/java/com/plusonetesting/rtdd/pointofsale/SellMultipleItemsTest.java
@@ -2,6 +2,7 @@ package com.plusonetesting.rtdd.pointofsale;
 
 import org.junit.Test;
 
+import java.awt.event.InputMethodEvent;
 import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
@@ -27,5 +28,17 @@ public class SellMultipleItemsTest {
         sale.onTotal();
 
         assertEquals("Total: $6.50", display.getText());
+    }
+
+    @Test
+    public void oneItemNotFound() {
+        Display display = new Display();
+        Catalog catalog = new Catalog(Collections.singletonMap("12345", "$6.50"));
+        Sale sale = new Sale(catalog, display);
+
+        sale.onBarcode("99999");
+        sale.onTotal();
+
+        assertEquals("No sale in progress. Try scanning a product.", display.getText());
     }
 }
