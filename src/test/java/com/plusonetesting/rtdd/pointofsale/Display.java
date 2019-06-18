24a29c600db98eb96118c82d7242b1bfe1d55fc9
Geir Gulbrandsen <dobbelg@gmail.com>
2019-06-18 14:35:41 +0100

    Pulled out Display to separate class

---
+++ src/test/java/com/plusonetesting/rtdd/pointofsale/Display.java
@@ -0,0 +1,21 @@
+package com.plusonetesting.rtdd.pointofsale;
+
+public class Display {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void displayPrice(String priceAsText) {
+        this.text = priceAsText;
+    }
+
+    public void displayProductNotFoundMessage(String barcode) {
+        this.text = "Product not found for " + barcode;
+    }
+
+    public void displayEmptyBarcodeMessage() {
+        this.text = "Scanning error: empty barcode";
+    }
+}
