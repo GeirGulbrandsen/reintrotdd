8ee123267b7e7c7270c83d10935551c264cd42b5
Geir Gulbrandsen <dobbelg@gmail.com>
2019-08-06 16:19:30 +0100

    We can now display a product not found message, but only for the magic barcode.

---
+++ src/test/java/com/plusonetesting/rtdd/clientfirstdesign/DisplayMessagesToConsoleTest.java
@@ -0,0 +1,47 @@
+package com.plusonetesting.rtdd.clientfirstdesign;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class DisplayMessagesToConsoleTest {
+
+    private PrintStream productionSystemOut;
+
+    @Before
+    public void rememberSystemOut() {
+        productionSystemOut = System.out;
+    }
+
+    @After
+    public void restoreSystemOut() {
+        System.setOut(productionSystemOut);
+    }
+
+    @Test
+    public void productNotFoundMessage() {
+        ByteArrayOutputStream canvas = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(canvas));
+
+        new ConsoleDisplay().displayProductNotFoundMessage("91837248");
+
+        Assert.assertEquals(
+                Arrays.asList("Product not found for 91837248"),
+                TextUtilities.lines(canvas.toString(StandardCharsets.UTF_8))
+        );
+    }
+
+    public static class ConsoleDisplay {
+
+        public void displayProductNotFoundMessage(String barcodeNotFound) {
+            System.out.println("Product not found for 91837248");
+        }
+    }
+}
