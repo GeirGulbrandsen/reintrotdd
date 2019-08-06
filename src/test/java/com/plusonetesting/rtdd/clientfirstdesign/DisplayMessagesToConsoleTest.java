f6fafce88e09667b7c7cf4f7e673eff4495dd3a8
Geir Gulbrandsen <dobbelg@gmail.com>
2019-08-06 17:13:10 +0100

    Cleaning up a little

--- src/test/java/com/plusonetesting/rtdd/clientfirstdesign/DisplayMessagesToConsoleTest.java
+++ src/test/java/com/plusonetesting/rtdd/clientfirstdesign/DisplayMessagesToConsoleTest.java
@@ -4,7 +4,6 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.w3c.dom.ls.LSOutput;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
