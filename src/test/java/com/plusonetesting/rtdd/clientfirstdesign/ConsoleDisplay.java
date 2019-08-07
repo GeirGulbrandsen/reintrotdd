a32c85a02fc8867f4c1fe00f11783d5886297bee
Geir Gulbrandsen <dobbelg@gmail.com>
2019-08-07 13:54:39 +0100

    Pulled out commonalities and separated the templating and rendering engines.

--- src/test/java/com/plusonetesting/rtdd/clientfirstdesign/ConsoleDisplay.java
+++ src/test/java/com/plusonetesting/rtdd/clientfirstdesign/ConsoleDisplay.java
@@ -2,19 +2,31 @@ package com.plusonetesting.rtdd.clientfirstdesign;
 
 public class ConsoleDisplay {
 
-    public static String formatPrice(Price cents) {
-        return String.format("$%,.2f", cents.dollarValue());
+    public static final String PRODUCT_NOT_FOUND_FOR_MESSAGE_FORMAT = "Product not found for %s";
+    public static final String SCANNING_ERROR_EMPTY_BARCODE_MESSAGE_FORMAT = "Scanning error: empty barcode.";
+    public static final String PRICE_IN_DOLLARS_MESSAGE_FORMAT = "$%,.2f";
+
+    private void render(String text) {
+        System.out.println(text);
+    }
+
+    private String mergeTemplate(String messageTemplate, Object... placeHolderValues) {
+        return String.format(messageTemplate, placeHolderValues);
+    }
+
+    public void displayMessage(String messageTemplate, Object... placeHolderValues) {
+        render(mergeTemplate(messageTemplate, placeHolderValues));
     }
 
     public void displayProductNotFoundMessage(String barcodeNotFound) {
-        System.out.println(String.format("Product not found for %s", barcodeNotFound));
+        displayMessage(PRODUCT_NOT_FOUND_FOR_MESSAGE_FORMAT, barcodeNotFound);
     }
 
     public void displayEmptyBarcodeMessage() {
-        System.out.println("Scanning error: empty barcode.");
+        displayMessage(SCANNING_ERROR_EMPTY_BARCODE_MESSAGE_FORMAT);
     }
 
     public void displayPrice(Price cents) {
-        System.out.println(formatPrice(cents));
+        displayMessage(PRICE_IN_DOLLARS_MESSAGE_FORMAT, cents.dollarValue());
     }
 }
