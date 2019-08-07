c36b8f855366663c2d29f9f10e06d70276a6731e
Geir Gulbrandsen <dobbelg@gmail.com>
2019-08-07 14:41:08 +0100

    Connected all three pieces for the first time, in production!

--- src/main/java/com/plusonetesting/rtdd/clientfirstdesign/VirtualPointOfSaleTerminal.java
+++ src/main/java/com/plusonetesting/rtdd/clientfirstdesign/VirtualPointOfSaleTerminal.java
@@ -1,8 +1,19 @@
 package com.plusonetesting.rtdd.clientfirstdesign;
 
+import java.util.HashMap;
+
 public class VirtualPointOfSaleTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SaleController saleController = new SaleController(
+                new InMemoryCatalog(new HashMap<String, Price>() {{
+                    put("12345", Price.cents(795));
+                    put("23456", Price.cents(1250));
+                }}),
+                new EnglishLanguageConsoleDisplay());
 
+        saleController.onBarcode("12345");
+        saleController.onBarcode("23345");
+        saleController.onBarcode("99999");
+        saleController.onBarcode("");
     }
 }
