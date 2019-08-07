f4f40fc2cdd69c0d2230fbc0f1da00c4c0a64058
Geir Gulbrandsen <dobbelg@gmail.com>
2019-08-07 10:52:23 +0100

    We can format prices expressed directly in cents.

---
+++ src/test/java/com/plusonetesting/rtdd/clientfirstdesign/DisplayPricesToConsoleTest.java
@@ -0,0 +1,48 @@
+package com.plusonetesting.rtdd.clientfirstdesign;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.runners.Parameterized.*;
+
+@RunWith(Parameterized.class)
+public class DisplayPricesToConsoleTest {
+
+    private final int priceInCents;
+    private final String expectedFormattedPrice;
+
+    public DisplayPricesToConsoleTest(int priceInCents, String expectedFormattedPrice) {
+        this.priceInCents = priceInCents;
+        this.expectedFormattedPrice = expectedFormattedPrice;
+    }
+
+    @Parameters(name = "Monetary amount {0} formats to {1}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {789, "$7.89"},
+                {520, "$5.20"},
+                {400, "$4.00"},
+                {0, "$0.00"},
+                {2, "$0.02"},
+                {37, "$0.37"},
+                {418976, "$4,189.76"},
+                {210832281, "$2,108,322.81"}
+        });
+    }
+
+
+    @Test
+    public void test() {
+        assertEquals(expectedFormattedPrice, format(priceInCents));
+    }
+
+    private static String format(int priceInCents) {
+        return String.format("$%,.2f", priceInCents/100.0d);
+    }
+}
+
